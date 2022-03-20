ef1e4cca13bbcbaae7a2e25bb596a6c7d5a40b8e
정희찬 <jung02466@naver.com>
2022-03-20 19:18:05 +0900

    commit message

---
+++ src/practice.java
@@ -0,0 +1,17 @@
+
+public class practice {
+
+	public static void main(String[] args) {
+		int distance = 100;
+		double time = 18;
+		double speed = distance/time;
+		
+		System.out.println("100미터 달리기 시속:"+speed);
+	   
+	    
+	    
+		// TODO Auto-generated method stub
+
+	}
+
+}
